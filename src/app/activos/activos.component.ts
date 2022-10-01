067d216fb28e9569a07d38784341d69ab1813bdd
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2022-10-01 16:37:07 -0500

    Cambios grupo esteban activos..
    -Asignación de custodio.
    -Responsable.
    -Departamento.

--- src/app/activos/activos.component.ts
+++ src/app/activos/activos.component.ts
@@ -1,3 +1,327 @@
+// import { Component, OnInit, Input } from '@angular/core';
+// import { CargarScriptsService } from './../cargar-scripts.service';
+// import { FormBuilder, FormControl } from '@angular/forms';
+// import { FormGroup, Validators } from '@angular/forms';
+// import { ActivosService } from 'src/app/service/activos.service';
+// import { DepartamentosService } from '../service/departamentos.service';
+// import { RegEdificioService } from '../service/reg-edificio.service';
+// import { ActivatedRoute } from '@angular/router';
+// import { RecepcionActivoService } from '../service/recepcion-activo.service';
+// import { Router } from '@angular/router';
+// import Swal from 'sweetalert2';
+// import { Activo } from '../models/activo';
+
+// import { StorageService } from '../service/storage.service';
+
+
+// declare var jQuery: any;
+// declare var $: any;
+// @Component({
+//   selector: 'app-activos',
+//   templateUrl: './activos.component.html',
+//   styleUrls: ['./activos.component.css'],
+// })
+// export class ActivosComponent implements OnInit {
+
+
+
+
+//   recepcionForm!: FormGroup;
+//   BuscarRecepForm!: FormGroup;
+//   listarActivoForm!: FormGroup;
+//   detalleRecep: any;
+//   public detalleAct: any;
+//   //para traer de la table
+//   activoForm!: FormGroup;
+//   activoCompletForm!: FormGroup;
+//   id: number = 0;
+//   activo: any;
+//   //para buscar por nume recep
+//   recep: any;
+//   num_recep: string = '';
+//   id_buscador: any;
+//   //*****//
+//   edificios: any;
+//   departamentos: any;
+
+//   constructor(
+//     public fb: FormBuilder,
+//     public reg: FormBuilder,
+//     public buscarRecep: ActivosService,
+//     private _CargaScripts: CargarScriptsService,
+//     public regedificioservice: RegEdificioService,
+//     public departamentoservice: DepartamentosService,
+//     private activateRoute: ActivatedRoute,
+//     private route: Router,
+
+//     private activoServe: ActivosService,
+//     private storageService: StorageService,
+
+//   ) {
+//     _CargaScripts.Carga(['js-activos/activos']);
+//   }
+
+//   public findUserByNumRecep() {
+//     let resp = this.activoServe.buscarNumRecepcion(this.num_recep);
+//     resp.subscribe((data) => (this.detalleAct = data));
+//   }
+
+//   //Verifcar si el user esta logiado..
+//   isLoggedIn = false;
+//   id_persona?: number; // valor del usuario que este log
+//   ngOnInit(): any {
+//     this.isLoggedIn = this.storageService.isLoggedIn();
+
+//     if (this.isLoggedIn) {
+//       console.log('Estamos logiados satisfacririamente')
+//       const user = this.storageService.getUser();
+//       this.id_persona = user.id;
+//     }
+
+//     this.buscarRecep.getAllDetalle_ing().subscribe(
+//       (detalleRecep) => {
+//         this.detalleRecep = detalleRecep;
+//         console.log(detalleRecep);
+//       },
+//       (error) => console.log(error)
+//     );
+
+//     // LLenar Edificios
+//     this.regedificioservice.getAllEdificios().subscribe(
+//       (edificios) => {
+//         this.edificios = edificios;
+//         console.log(edificios);
+//       },
+//       (error) => console.log(error)
+//     );
+
+
+//   }
+
+
+
+//   // NEW VERSION
+
+
+//   onSubmit() {
+//     this.activoServe.traerDatos(this.id, this.activo).subscribe(
+//       (data) => console.log(data),
+//       (error) => console.log(error)
+//     );
+//     this.activo = new Activo();
+//     this.route.navigate(['/Gestionar_activos']);
+//   }
+
+//   ////update
+//   traerDatos(id: number) {
+//     this.route.navigate(['Gestion_activos', id]);
+//   }
+
+//   // Cargar Departamentes Anidado
+//   onSelect(event: any) {
+//     let id_edificio = event.target.value;
+//     console.log(id_edificio);
+//     this.departamentoservice.getByidDepartamentos(id_edificio).subscribe(
+//       (resp) => {
+//         console.log(resp)
+//         this.departamentos = resp;
+//       },
+//       (error) => console.error(error)
+//     );
+//   }
+
+//   Finaliza(): void {
+//     // this.recepcionFormDetalle.reset();
+//     // this.recepcionForm.reset();
+
+//     // // Cargar tabla vacia
+//     // let id_encabezado_ing_vacia = 0;
+//     // this.recepcionservice.getDatosEncabezado(id_encabezado_ing_vacia).subscribe(
+//     //   resp => {
+//     //     this.detalleEnc = resp;
+//     //   },
+//     //   error => (console.error(error))
+//     // )
+//     Swal.fire('Activo Registrado Correctamente', 'Continue', 'success');
+//   }
+
+
+//   salida: string = "";
+//   id_detalle: number;
+//   //cap_id_encabezado: number;
+//   pasarCodigoActivo(cod_pro, id_detallea, id_encabezadoa_ing) {
+//     this.salida = cod_pro;
+//     // alert(id_detallea)
+//     this.value = (document.getElementById("CodigoActivo") as HTMLInputElement).value;
+//     this.value = this.salida;
+//     this.id_detalle = id_detallea;
+//     this.cap_id_encabezado = id_encabezadoa_ing;
+//     console.log(this.id_detalle)
+//     console.log("este es el id del enca=> " + this.cap_id_encabezado)
+//   }
+
+//   @Input()
+//   value: string = "";
+//   estadoA: string = '';
+//   dispocicionAc: boolean;
+
+
+//   form: any = {
+//     id_activo: null,
+//     codigo_activo: null,
+//     nombre: null,
+//     serie: null,
+//     marca: null,
+//     modelo: null,
+//     imagen: null,
+//     descripcion: null,
+//     costo: null,
+//     estado_fisico: this.estadoA,
+//     disponibilidad: null,
+//   };
+
+//   listaEstado: string[] = ["Bueno", "Malo", "dañado"];
+//   listaDispocicion: string[] = ["Ocupado", "Disponible"];
+
+//   estadoActivo(e) {
+//     console.log(e.target.value)
+//     this.estadoA = e.target.value;
+//   }
+
+//   dispocicionActivo(e) {
+//     console.log(e.target.value)
+//     let valorRes = e.target.value;
+//     if (valorRes == 'Ocupado') {
+//       this.dispocicionAc = false;
+//     }
+//     if (valorRes == 'Disponible') {
+//       this.dispocicionAc = true;
+//     }
+//   }
+
+//   //Resgitro del activo
+//   id_activo1: number;
+//   registroAc(): void {
+//     const { nombre, serie, marca, modelo, imagen, descripcion, costo } = this.form;
+//     this.activoServe.GuardarAc(this.value, nombre, serie, marca, modelo, imagen, descripcion, costo, this.estadoA, this.dispocicionAc).subscribe({
+//       next: data => {
+//         this.id_activo1 = data.id_activo;
+//         console.log(data);
+//         console.log('Codigo de ingreso-> ' + this.id_activo1);
+//         console.log('Correcto el ingreso del activo')
+//         this.guardarHistorialActivo();
+//         this.actualizarDetalleEstado();
+//       },
+//       error: err => {
+//         console.log('Error no se pudo guardar el activo')
+//       }
+//     });
+//   }
+
+//   // historial
+//   id_departamento: number;
+//   ids_Departamento(e) {
+//     this.id_departamento = e.target.value;
+//     console.log('Id del departameno-> ' + this.id_departamento)
+//   }
+
+//   guardarHistorialActivo(): void {
+
+//     let historial_ing = {
+//       "estado_fisico": this.estadoA,
+//       "id_activo": {
+//         "id_activo": this.id_activo1
+//       },
+//       "id_departamento": {
+//         "id_departamento": this.id_departamento
+//       },
+//       "id_detalle_ing": {
+//         "id_detalle_ing": this.id_detalle
+//       },
+//       "id_responsable": {
+//         "id_usuario": this.id_persona
+//       }
+
+//     };
+
+//     this.activoServe.saveHistorialActivos(historial_ing).subscribe({
+//       next: data => {
+//         console.log(data);
+//         console.log('Correcto el ingreso del del historial del detalle de activo')
+//       },
+//       error: err => {
+//         console.log('Error no se pudo guardar del historial del detalle de activo')
+//       }
+//     });
+//   }
+
+//   actualizarDetalleEstado(): void {
+
+//     let detalle_ing = {
+//       "id_detalle_ing": this.id_detalle,
+//       "estado_detalle": true,
+//       "encabezado_ing": {
+//         "id_encabezado_ing": 0
+//       }
+//     };
+
+//     this.activoServe.updateDetalleEstadoDeIngreso(detalle_ing).subscribe({
+//       next: data => {
+//         console.log(data);
+//         console.log('Se actualizo el detalle')
+//         this.verificarElConteoDeDatos();
+//       },
+//       error: err => {
+//         console.log('Error al actualizar el detalle')
+//         this.verificarElConteoDeDatos();
+//       }
+//     });
+//   }
+
+//   cap_id_encabezado: number;
+//   conteo: number;
+//   verificarElConteoDeDatos(): void {
+//     this.activoServe.conteoDeDatosDelDetalle(this.cap_id_encabezado).subscribe({
+//       next: data => {
+//         console.log('Este es el id del enca a enviar' + this.cap_id_encabezado)
+//         console.log(data);
+//         this.conteo = data;
+//         console.log('Este es los ingresados ' + this.conteo)
+//         console.log('Esto se va a comparar ' + this.conteo)
+//         if (this.conteo == 0) {
+//           this.actualizarEncabezadoEstado();
+//         } else {
+//           console.log('Falta de ingresar ' + this.conteo)
+//         }
+//       },
+//       error: err => {
+//         console.log('Error al acapturar el conteo')
+//       }
+//     });
+//   }
+
+
+//   actualizarEncabezadoEstado(): void {
+
+//     let detalle_ing = {
+//       "id_encabezado_ing": this.cap_id_encabezado,
+//       "estado": true,
+//     };
+
+//     this.activoServe.updateEncabezadoEstadoDeIngreso(detalle_ing).subscribe({
+//       next: data => {
+//         console.log(data);
+//         console.log('Se actualizo el encabezado')
+//       },
+//       error: err => {
+//         console.log('Error al actualizar el encabezado')
+//       }
+//     });
+//   }
+
+
+// }
+
 import { Component, OnInit, Input } from '@angular/core';
 import { CargarScriptsService } from './../cargar-scripts.service';
 import { FormBuilder, FormControl } from '@angular/forms';
@@ -12,6 +336,8 @@ import Swal from 'sweetalert2';
 import { Activo } from '../models/activo';
 
 import { StorageService } from '../service/storage.service';
+import { Observable } from 'rxjs';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
 
 
 declare var jQuery: any;
@@ -25,6 +351,12 @@ export class ActivosComponent implements OnInit {
 
 
 
+    selectedFiles?: FileList;
+    currentFile?: File;
+    progress = 0;
+    message = '';
+  
+    fileInfos?: Observable<any>;
 
   recepcionForm!: FormGroup;
   BuscarRecepForm!: FormGroup;
@@ -57,10 +389,11 @@ export class ActivosComponent implements OnInit {
     private activoServe: ActivosService,
     private storageService: StorageService,
 
+
   ) {
     _CargaScripts.Carga(['js-activos/activos']);
   }
-
+ 
   public findUserByNumRecep() {
     let resp = this.activoServe.buscarNumRecepcion(this.num_recep);
     resp.subscribe((data) => (this.detalleAct = data));
@@ -73,7 +406,7 @@ export class ActivosComponent implements OnInit {
     this.isLoggedIn = this.storageService.isLoggedIn();
 
     if (this.isLoggedIn) {
-      console.log('Estamos logiados satisfacririamente')
+      console.log('Estamos logiados Satisfactoriamente')
       const user = this.storageService.getUser();
       this.id_persona = user.id;
     }
@@ -98,12 +431,14 @@ export class ActivosComponent implements OnInit {
 
   }
 
+  
 
 
   // NEW VERSION
 
 
   onSubmit() {
+
     this.activoServe.traerDatos(this.id, this.activo).subscribe(
       (data) => console.log(data),
       (error) => console.log(error)
@@ -211,6 +546,11 @@ export class ActivosComponent implements OnInit {
         console.log('Correcto el ingreso del activo')
         this.guardarHistorialActivo();
         this.actualizarDetalleEstado();
+
+        // new
+        this.guardarAsignacionDeDepartamento();
+        this.guardarAsignacionDeResponsable();
+        this.guardarAsignacionDeCustodio();
       },
       error: err => {
         console.log('Error no se pudo guardar el activo')
@@ -306,6 +646,9 @@ export class ActivosComponent implements OnInit {
     let detalle_ing = {
       "id_encabezado_ing": this.cap_id_encabezado,
       "estado": true,
+      // "file": {
+      //   "id_documento": null
+      // }
     };
 
     this.activoServe.updateEncabezadoEstadoDeIngreso(detalle_ing).subscribe({
@@ -318,6 +661,159 @@ export class ActivosComponent implements OnInit {
       }
     });
   }
+
+
+  //**************************** */
+  selectFile(event: any): void {
+    this.selectedFiles = event.target.files;
+  }
+
+  cap_id_documento: string;
+  cap_nombre_documento: string;
+
+  cap_nombre_documento2: any;
+  cap_nombre_documento3: any;
+  new_cap_nombre_documento3: any;
+
+  caps_nombre_documento(e) {
+    this.caps_nombre_documento = e.target.value;
+    console.log('Nombre del archivo-> ' + this.caps_nombre_documento)
+    console.log('nuemor de datos del archivo-> ' +this.caps_nombre_documento.length)
+    this.cap_nombre_documento2 = this.caps_nombre_documento;
+    console.log('el nuev quitado-> ' + this.cap_nombre_documento2.slice(12))
+    this.cap_nombre_documento3 = this.cap_nombre_documento2.slice(12);
+    this.new_cap_nombre_documento3 = this.cap_nombre_documento3.slice(2,-2)
+    console.log('el id = ' + this.new_cap_nombre_documento3)
+    
+  }
+
+
+  upload(): void {
+    this.progress = 0;
+  
+    if (this.selectedFiles) {
+      const file: File | null = this.selectedFiles.item(0);
+  
+      if (file) {
+        this.currentFile = file;
+  
+        this.activoServe.upload(this.currentFile).subscribe(
+          (event: any) => {
+            if (event.type === HttpEventType.UploadProgress) {
+              this.cap_id_documento = event.id_documento;
+              console.log("la id del docuemnto =>" + this.cap_id_documento)
+              console.log();
+              this.progress = Math.round(100 * event.loaded / event.total);
+              this.message = 'Se ha subido el acta, exitosamente';
+            } else if (event instanceof HttpResponse) {
+              this.message = event.body.message;
+              this.message = 'No se ha podido subir el acta';
+            }
+          });
+      }
+      this.selectedFiles = undefined;
+    }
+  }
+
+  cap_id_del_documento: any;
+  new_cap_id_del_documento: number;
+
+  // NAMESSSSSSSSS
+  buscarElNameDoc(): void{
+    this.activoServe.buscarIdporNombreDelDoc(this.cap_nombre_documento3).subscribe({
+      next: data => {
+        console.log('esto se va a enviar_' + this.cap_nombre_documento3)
+        console.log(data)
+        console.log('Esta es la id del doc =>' + data)
+        this.cap_id_del_documento = data;
+        //this.new_cap_id_del_documento = this.cap_id_del_documento.slice(1, -1)
+        console.log('Esta es la id del doc 2 =>' + this.cap_id_del_documento)
+      },
+      error: err => {
+        console.log('no hay id')
+      }
+    });
+  }
+  //****************** */
+
+
+  // Nuevo guardados por defecto
+
+  guardarAsignacionDeDepartamento(): void {
+
+    console.log('id depa' + this.id_departamento)
+
+    let asignacion_departamento = {
+
+        "id_asignacion_Responsable": 0,
+        "fecha_fin": null,
+        "id_activo": {
+          "id_activo":  this.id_activo1,
+        },
+        "id_departamento": {
+          "id_departamento": this.id_departamento,
+        },
+    };
+
+    this.activoServe.saveDepartamento(asignacion_departamento).subscribe({
+      next: data => {
+        console.log(data);
+        console.log('Correcto el departamento por defecto')
+      },
+      error: err => {
+        console.log('Error no se pudo guardar del historial del detalle de activo')
+      }
+    });
+  }
+
+  guardarAsignacionDeResponsable(): void {
+
+    let asignacion_responsable = {
+        "fecha_fin": null,
+        "id_activo": {
+          "id_activo":  this.id_activo1,
+        },
+        "id_Uresponsable": {
+          "id_usuario":  this.id_persona
+        }
+    };
+
+    this.activoServe.saveResponsable(asignacion_responsable).subscribe({
+      next: data => {
+        console.log(data);
+        console.log('Correcto el responsable por defecto')
+      },
+      error: err => {
+        console.log('Error no se pudo guardar del historial del detalle de activo')
+      }
+    });
+  }
+
+  guardarAsignacionDeCustodio(): void {
+
+    let asignacion_custodio = {
+        "fecha_fin": null,
+        "id_activo": {
+          "id_activo":  this.id_activo1,
+        },
+        "id_Ucustodio": {
+          "id_usuario":  this.id_persona
+        }
+    };
+
+    this.activoServe.saveCustodio(asignacion_custodio).subscribe({
+      next: data => {
+        console.log(data);
+        console.log('Correcto el custodio por defecto')
+      },
+      error: err => {
+        console.log('Error no se pudo guardar del historial del detalle de activo')
+      }
+    });
+  }
+
+
+  //////
 
 
 }
--- src/app/registrar/registrar.component.ts
+++ src/app/registrar/registrar.component.ts
@@ -15,7 +15,6 @@ import Swal from 'sweetalert2';
 //Import de las rutas..
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-registrar',
   templateUrl: './registrar.component.html',
@@ -24,91 +23,125 @@ import { Router } from '@angular/router';
 export class FormComponent implements OnInit {
   public usuario: Usuarios = new Usuarios();
 
+  estadoContra: boolean = false;
+
+
   constructor(
     private router: Router,
     private createAccountService: CreateAccountService,
     private bd_fenix: BaseFenixService
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   public create(): void {
-
-    let interceptor = this.usuario.correo.split('@');
-
-    let dominio = interceptor[1];
-
-    if (dominio === 'tecazuay.edu.ec') {
-
-      this.createAccountService.createUser(this.usuario).subscribe(
-        data => {
-          Swal.fire({
-            position: 'top',
-            icon: 'success',
-            title: 'Cuenta creada satisfactoriamente.',
-            showConfirmButton: false,
-            timer: 2000
-          })
-          this.router.navigate(['/login'])
-        },
-        err => {
-          console.log('Lo que me viene en el err --> ' + err.error.message);
-
-          switch(err.error.message){
-            case 'Error: Usuario ya esta en la BD!':
-              Swal.fire(
-                'No se puedo crear el usuario',
-                `Este usuario ya esta registrado`,
-                'error'
-              );
-            break;
-
-            case 'Error: El usuario no esta en FENIX!':
-              Swal.fire(
-                'Docente no registrado en FENIX',
-                `Este docenete no se encuentra registrado en fenix.`,
-                'error'
-              );
-            break;
-
-            case 'Error: Usted no puede ingresar un correo existente!':
-              Swal.fire(
-                'Correo institucional ya registrado',
-                `El correo ingresaso ya eta registrado con otro docente.`,
-                'error'
-              );
-            break;
-          }
-
-        }
-      )
-
+    if (this.estadoContra == false) {
+      Swal.fire({
+        text: 'Contraseña no cumple con los requisitos de seguridad.',
+        icon: 'error',
+        confirmButtonText: 'Cool',
+      });
     } else {
-      Swal.fire(
-        'El correo no es institucional',
-        `Corre incorrecto`,
-        'warning'
-      )
+      let interceptor = this.usuario.correo.split('@');
+
+      let dominio = interceptor[1];
+
+      if (dominio === 'tecazuay.edu.ec') {
+        this.createAccountService.createUser(this.usuario).subscribe(
+          (data) => {
+            Swal.fire({
+              position: 'top',
+              icon: 'success',
+              title: 'Cuenta creada satisfactoriamente.',
+              showConfirmButton: false,
+              timer: 2000,
+            });
+            this.router.navigate(['/login']);
+          },
+          (err) => {
+            console.log('Lo que me viene en el err --> ' + err.error.message);
+
+            switch (err.error.message) {
+              case 'Error: Usuario ya esta en la BD!':
+                Swal.fire(
+                  'No se puedo crear el usuario',
+                  `Este usuario ya esta registrado`,
+                  'error'
+                );
+                break;
+
+              case 'Error: El usuario no esta en FENIX!':
+                Swal.fire(
+                  'Docente no registrado en FENIX',
+                  `Este docenete no se encuentra registrado en fenix.`,
+                  'error'
+                );
+                break;
+
+              case 'Error: Usted no puede ingresar un correo existente!':
+                Swal.fire(
+                  'Correo institucional ya registrado',
+                  `El correo ingresaso ya eta registrado con otro docente.`,
+                  'error'
+                );
+                break;
+            }
+          }
+        );
+      } else {
+        Swal.fire(
+          'El correo no es institucional',
+          `Corre incorrecto`,
+          'warning'
+        );
+      }
     }
   }
 
+  validarCoontra(evento: any) {
+    let password = evento.target.value;
 
+    if (
+      password.length >= 6 &&
+      password.match(/.*[A-Z]/) &&
+      password.match(/.*[a-z]/)
+    ) {
+      console.log('Contra correcta..');
+      this.estadoContra = true;
+    } else {
+      this.estadoContra = false;
+      console.log('Fail..');
+    }
+    this.geteventOculta()
+  }
+
+  geteventOculta() {
+    const spamm = document.getElementById('elemento') as HTMLButtonElement | null;
+    if (spamm != null) {
+
+      if (this.estadoContra == false) {
+        spamm.style.display = 'block';
+      }
+
+      if (this.estadoContra == true) {
+        spamm.style.display = 'none';
+      }
+    }
+  }
 
   //Vamos a manejar el evento de busqueda..
   correoe = document.getElementById('correo');
   public personas: Verpersonaf = new Verpersonaf();
 
-  public consultaUserFenix(ci: string): void { 
+  public consultaUserFenix(ci: string): void {
     this.bd_fenix.consultarUser(ci).subscribe((response) => {
       this.usuario.nombre = response.nombres;
       this.usuario.apellido = response.apellidos;
       this.usuario.correo = response.correo_institucional;
     });
-
   }
 
-  escribirInput(evento:any) {
-
+  escribirInput(evento: any) {
     let cedulaF = evento.target.value;
 
     if (cedulaF.length == 10) {
@@ -150,19 +183,20 @@ export class FormComponent implements OnInit {
   }
 
   //Metodo que nos servira para autocompletar con el tecazuay
-  autoEmail(evento:any){
-    let cedulaF = evento.target.value; 
+  autoEmail(evento: any) {
+    let cedulaF = evento.target.value;
 
-    let valor = cedulaF.substr(-1)
+    let valor = cedulaF.substr(-1);
 
     cedulaF = cedulaF.trim();
 
     let cedulafinal = cedulaF.split(' ').join('');
 
-    this.usuario.correo= cedulafinal;
-    if(valor == '@'){
-      console.log(cedulaF)
-      this.usuario.correo = cedulafinal+'tecazuay.edu.ec';
+    this.usuario.correo = cedulafinal;
+    if (valor == '@') {
+      console.log(cedulaF);
+      this.usuario.correo = cedulafinal + 'tecazuay.edu.ec';
     }
   }
+
 }
